5eaed08fb9516e806b74fe6e8b558da0a16314aa
Rahul <rahul@sgcms.com>
2024-12-12 17:45:02 +0530

    changes

---
+++ controller/Shopify/Customer.js
@@ -0,0 +1,25 @@
+const app = require("express").Router();
+
+const axios = require("axios");
+app.post("/CustomerOrders", (req, res) => {
+    
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `https://${req.body.StoreName}/admin/api/2024-10/customers/${req.body.CustomerId}/orders.json`,
+    headers: {
+      "X-Shopify-Access-Token": process.env.Shopify_Token,
+    },
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      res.json(response.data)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+module.exports = app
